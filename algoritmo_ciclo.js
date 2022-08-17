122eb03fd660df2bd2eb6f0852cee1a9152fff0b
santi0995 <santiserrato0910@gmail.com>
2022-08-18 00:36:09 +0200

    Cambios

---
+++ algoritmo_ciclo.js
@@ -0,0 +1,33 @@
+ let bandera = 0;
+
+ do{
+     if(bandera == 0){
+         console.log("S");
+         bandera++;
+     } if (bandera == 1){
+         console.log("A");
+         bandera++;
+    
+     } if (bandera == 2){
+         console.log("N");
+         bandera++;
+    
+     } if (bandera == 3){
+         console.log("T");
+         bandera++;
+    
+     } if (bandera == 4){
+         console.log("I");
+         bandera++;
+     } if (bandera == 5){
+         console.log("A");
+         bandera++;
+     } if (bandera == 6){
+         console.log("G");
+         bandera++;
+     } if (bandera == 7){
+         console.log("O");
+         bandera++;
+     }
+ } while(bandera == 7);
+
--- app.js
+++ app.js
@@ -11,39 +11,51 @@
 //     alert("401 Unauthorized")
 // }
 
-const name = prompt("Nombre");
-const documento = prompt("Número de documento de identificación");
-const ingresos = parseInt(prompt("Ingresos totales mensuales"));
-const gastos = parseInt(prompt("Suma de gastos mensuales"));
-const tieneTarjeta = prompt("Tiene tarjeta?");
-let cupo;
-if(tieneTarjeta == "Si" ){
-     cupo = parseInt(prompt("cupo"));
-}
-const tipoEmpleado= prompt("Tipo de empleado");
-let actividad;
-let tipoDeContrato = "contratista";
-if(tipoEmpleado == "empleado"){
-    tipoDeContrato  == prompt("Tipo de contrato");
-} else if(tipoEmpleado == "independiente"){
-    actividad  = parseInt(prompt("Tipo de actividad"));
-} 
+// const name = prompt("Nombre");
+// const documento = prompt("Número de documento de identificación");
+// const ingresos = parseInt(prompt("Ingresos totales mensuales"));
+// const gastos = parseInt(prompt("Suma de gastos mensuales"));
+// const tieneTarjeta = prompt("Tiene tarjeta?");
+// let cupo;
+// if(tieneTarjeta == "Si" ){
+//      cupo = parseInt(prompt("cupo"));
+// }
+// const tipoEmpleado= prompt("Tipo de empleado");
+// let actividad;
+// let tipoDeContrato = "contratista";
+// if(tipoEmpleado == "empleado"){
+//     tipoDeContrato  == prompt("Tipo de contrato");
+// } else if(tipoEmpleado == "independiente"){
+//     actividad  = parseInt(prompt("Tipo de actividad"));
+// } 
 
 
 
 
-if(tieneTarjeta == "Si" && cupo >= ingresos*2.5){
-    console.log("Su tarjeta ha sido rechazada");
-} 
-if(ingresos - gastos < ingresos/2){
-    console.log("Su tarjeta ha sido rechazada");
-}
-if(tipoEmpleado == "independiente" && actividad <= 3 && ingresos < 1000){
-    console.log("Su tarjeta ha sido rechazada");
-}
-if(tipoEmpleado == "empleado" && tipoDeContrato == "contratista" &&  ingresos < 500){
-    alert("Su tarjeta ha sido rechazada");
-}
-if(documento % 2 != 0){
-    console.log("Su tarjeta ha sido rechazada");
-}
+// if(tieneTarjeta == "Si" && cupo >= ingresos*2.5){
+//     console.log("Su tarjeta ha sido rechazada");
+// } 
+// else if(ingresos - gastos < ingresos/2){
+//     console.log("Su tarjeta ha sido rechazada");
+// }
+// else if(tipoEmpleado == "independiente" && actividad <= 3 && ingresos < 1000){
+//     console.log("Su tarjeta ha sido rechazada");
+// }
+// else if(tipoEmpleado == "empleado" && tipoDeContrato == "contratista" &&  ingresos < 500){
+//     alert("Su tarjeta ha sido rechazada");
+// }
+// else if(documento % 2 != 0){
+//     console.log("Su tarjeta ha sido rechazada");
+// } else{
+//     if(tieneTarjeta != "Si" && ingresos > 1000 && gastos <= 600 && tipoEmpleado == "empleado"){
+//         cupo = 2000
+//         console.log(`El cupo aprobado es de ${cupo} dolares`)
+//     } else if (tieneTarjeta != "Si" && ingresos > 2000 && gastos <= 600 && tipoEmpleado == "empleado" || tipoEmpleado == "independiente" ) {
+//         cupo = 3000
+//         console.log(`El cupo aprobado es de ${cupo} dolares`)
+//     } else{
+//         cupo = 1200
+//         console.log(`Cupo minimo aprobado de ${cupo} dolares`)
+//     }
+// }
+
